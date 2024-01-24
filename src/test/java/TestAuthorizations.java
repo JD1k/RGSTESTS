fe286cd7841afceb2977d2452d34cb1f86d0e6a0
JD1K <maksimqa.makaroff2014@yandex.ru>
2024-01-24 20:49:18 +0300

    выполнил первое задание

---
+++ src/test/java/TestAuthorizations.java
@@ -0,0 +1,15 @@
+import com.codeborne.selenide.Condition;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.open;
+
+public class TestAuthorizations {
+    @Test
+    public void TestBrowser(){
+        open("https://sravni.id/");
+        $x("//a[@class='eJpgv4rrNlWX9wgPQYHg _p7lcln _7k5cz5']").click();
+        $x("//input[@name='email']").setValue("maksimqa.makaroff2014@yandex.ru").shouldBe(Condition.value("maksimqa.makaroff2014@yandex.ru")).pressEnter();
+        $x("//input[@name='password']").setValue("12EArsRUsh").shouldBe(Condition.value("12EArsRUsh")).pressEnter();
+    }
+}
