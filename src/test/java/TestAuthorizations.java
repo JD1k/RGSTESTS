2f05a200627e93640152cbe06cd1b6cd839fecd5
JD1K <maksimqa.makaroff2014@yandex.ru>
2024-02-01 12:52:37 +0300

    выполнил первое задание)

--- src/test/java/TestAuthorizations.java
+++ src/test/java/TestAuthorizations.java
@@ -1,4 +1,5 @@
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Selenide;
 import org.testng.annotations.Test;
 
 import static com.codeborne.selenide.Selenide.$x;
@@ -11,5 +12,8 @@ public class TestAuthorizations {
         $x("//a[@class='eJpgv4rrNlWX9wgPQYHg _p7lcln _7k5cz5']").click();
         $x("//input[@name='email']").setValue("maksimqa.makaroff2014@yandex.ru").shouldBe(Condition.value("maksimqa.makaroff2014@yandex.ru")).pressEnter();
         $x("//input[@name='password']").setValue("12EArsRUsh").shouldBe(Condition.value("12EArsRUsh")).pressEnter();
+        $x("//a[@href='https://www.sravni.ru/']").click();
+        $x("//h1[@class='_e9qrci _15bmsvx']").shouldHave(Condition.text("Помогаем сохранить деньги"));
+
     }
 }
