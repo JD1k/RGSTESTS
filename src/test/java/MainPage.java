62520ee6cab07109f43d61c25b95c5dd71b9ec3a
JD1K <maksimqa.makaroff2014@yandex.ru>
2024-03-01 22:38:51 +0300

    выполнил первое задание)

---
+++ src/test/java/MainPage.java
@@ -0,0 +1,9 @@
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
+
+public class MainPage {
+    protected static final SelenideElement pickOsago = $x("//h6[@class='Card_cardTitle___wX6w _e9qrci _wf06uq']");
+
+}
